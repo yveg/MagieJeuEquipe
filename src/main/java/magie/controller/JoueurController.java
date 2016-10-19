f2810d4a61fbb6e7e40a4779516407bfd8831df4
yveg <gambaly@gmail.com>
2016-10-19 14:43:25 +0200

    Merge origin/master
    Conflicts:
    	MagieMagie2.mdj

--- src/main/java/magie/controller/JoueurController.java
+++ src/main/java/magie/controller/JoueurController.java
@@ -31,21 +31,22 @@ public class JoueurController {
         return "home.jsp";
     }
      */
-    @RequestMapping(value = {"/creerjoueur"}, method = RequestMethod.POST)
+    @RequestMapping(value = "/creerjoueur", method = RequestMethod.POST)
     public String cookitpPOST(Joueur j, HttpSession couqui) {
         serv.save(j);
         couqui.setAttribute("nomjj", j.getId());
         return "redirect:/lister_parties";
     }
-    
-      @RequestMapping(value = {"/creerjoueur"}, method = RequestMethod.GET)
+    /*
+      @RequestMapping(value = "/creerjoueur", method = RequestMethod.GET)
     public String cookitpGET(Joueur j, HttpSession couqui) {
         serv.save(j);
         couqui.setAttribute("nomjj", j.getId());
         return "redirect:/lister_parties";
     }
+*/
 
-    @RequestMapping(value = {"/attentejoueur"}, method = RequestMethod.GET)
+    @RequestMapping(value = "/attentejoueur", method = RequestMethod.GET)
     public String listerattentejoueurGET(Model m,HttpSession couqui) {
        // m.addAttribute("nomjoueur", serv.findAllByOrderByIdAsc());
          m.addAttribute("nomjoueur", serv.findAll());
