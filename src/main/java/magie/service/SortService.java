7ec17b8a973b2f2be2d9058a9d8a6ab4069e1e20
ellawy633 <abd.alay@hotmail.com>
2016-10-21 10:54:29 +0200

    sort

--- src/main/java/magie/service/SortService.java
+++ src/main/java/magie/service/SortService.java
@@ -5,6 +5,13 @@
  */
 package magie.service;
 
+import static java.lang.Math.floor;
+import java.util.ArrayList;
+import java.util.List;
+import magie.entity.Ingredient;
+import magie.entity.Joueur;
+import org.springframework.beans.factory.annotation.Autowired;
+
 /**
  *
  * @author admin
@@ -12,8 +19,36 @@ package magie.service;
 public class SortService {
     
     
-    
+  @Autowired JoueurServiceCrud crudJoueur;
+  @Autowired IngredientServiceCrud crudIngredients; 
   
+private JoueurServiceCrud crudSort;
+  
+  
+    
+  public void invisibilitie(Long joueurlance, Long vectime ){
+      
+   List<Joueur> joueurs = (List) crudJoueur.findAll();
+   
+      List<Ingredient> ingredients =(List) joueurs.get(0).getIngredients() ;
+     Math.floor(Math.random()*7);
+     
+      //ingredients.add(floor(Math.random()*7));
+      
+      
+      
+      
+          ingredients.addAll(ingredients);
+                  crudJoueur.findOne(1L).getIngredients();
+           
+      
+       
+       
+         
+          
+         
+     
+  }
     
     
     
